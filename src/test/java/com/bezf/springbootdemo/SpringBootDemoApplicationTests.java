6698b3271e4c62451adabf4ef8d7281c822bc15c
VeraLizunova <vera.lizunova.98@mail.ru>
2023-01-18 18:04:01 +0100

    fixed

--- src/test/java/com/bezf/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/bezf/springbootdemo/SpringBootDemoApplicationTests.java
@@ -1,15 +1,17 @@
 package com.bezf.springbootdemo;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootDemoApplicationTests {
     @Autowired
@@ -19,23 +21,21 @@ class SpringBootDemoApplicationTests {
     private final static String START_URL = "http://localhost:";
     private final static String DEV_EXPECTED = "Current profile is dev";
     private final static String PRODUCTION_EXPECTED = "Current profile is production";
-    private final GenericContainer<?> firstApp = new GenericContainer<>("devapp:latest")
+    private final static String ENDPOINT = "/profile";
+    ;
+    @Container
+    private final GenericContainer<?> firstApp = new GenericContainer<>("devappnew:latest")
             .withExposedPorts(FIRST_APP_PORT);
-    private final GenericContainer<?> secondApp = new GenericContainer<>("prodapp:latest")
+    @Container
+    private final GenericContainer<?> secondApp = new GenericContainer<>("prodappnew:latest")
             .withExposedPorts(SECOND_APP_PORT);
-
-    @BeforeEach
-    void setUp() {
-        firstApp.start();
-        secondApp.start();
-    }
 
     @Test
     @DisplayName("when the current profile is dev then \"Current profile is dev\" should be returned")
     void returnStringWhenCurrentProfileIsDev() {
         var expected = DEV_EXPECTED;
         var mappedPort = firstApp.getMappedPort(FIRST_APP_PORT);
-        var forEntity = restTemplate.getForEntity(START_URL + mappedPort, String.class);
+        var forEntity = restTemplate.getForEntity(START_URL + mappedPort + ENDPOINT, String.class);
 
         var actual = forEntity.getBody();
 
@@ -47,8 +47,8 @@ class SpringBootDemoApplicationTests {
     @DisplayName("when the current profile is production then \"Current profile is production\" should be returned")
     void returnStringWhenCurrentProfileIsProduction() {
         var expected = PRODUCTION_EXPECTED;
-        var mappedPort = firstApp.getMappedPort(SECOND_APP_PORT);
-        var forEntity = restTemplate.getForEntity(START_URL + mappedPort, String.class);
+        var mappedPort = secondApp.getMappedPort(SECOND_APP_PORT);
+        var forEntity = restTemplate.getForEntity(START_URL + mappedPort + ENDPOINT, String.class);
 
         var actual = forEntity.getBody();
 
